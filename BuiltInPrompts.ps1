e9f0e54c612172687f824d3b601378b7d82b8021
jimmehc <jimmehc@github.com>
2015-08-04 23:27:04 -0700

    Add a comment to each built in prompt element to make it easier to see what each does when Get-PromptElements is run.

--- BuiltInPrompts.ps1
+++ BuiltInPrompts.ps1
@@ -16,26 +16,26 @@ function script:Get-DefaultPathOutput
 }
 
 # Prompt element definitions
-function pe_UNAtCN { 
+function pe_UNAtCN { <#UNAtCN #> 
     Write-Host -ForeGroundColor $script:promptColors["Preamble"] "$($env:USERNAME)@$($env:COMPUTERNAME)".ToLower() -NoNewLine; 
 }
 
-function pe_UNAtCNBrackets { 
+function pe_UNAtCNBrackets { <#UNAtCNBrackets #> 
     Write-Host -ForeGroundColor $script:promptColors["Brackets"] "[" -NoNewLine
     pe_UNAtCN
     Write-Host -ForeGroundColor $script:promptColors["Brackets"] "]" -NoNewLine
 }
 
-function pe_FullPath {
+function pe_FullPath { <#FullPath #>
     Write-Host -ForeGroundColor $script:promptColors["Path"] (script:Get-DefaultPathOutput) -NoNewLine
 }
 
-function pe_UNCNWithPath{
+function pe_UNCNWithPath{ <#UNCNWithPath#>
     pe_UNAtCN
     pe_FullPath
 }
 
-function pe_UNCNWithPathAndBrackets{
+function pe_UNCNWithPathAndBrackets{ <#UNCNWithPathAndBrackets#>
     Write-Host -ForeGroundColor $script:promptColors["Preamble"] "[" -NoNewLine
     pe_UNAtCN
     Write-Host -ForeGroundColor $script:promptColors["Preamble"] ":" -NoNewLine
@@ -43,27 +43,27 @@ function pe_UNCNWithPathAndBrackets{
     Write-Host -ForeGroundColor $script:promptColors["Path"] "]" -NoNewLine
 }
 
-function pe_NewLine {
+function pe_NewLine { <#NewLine #>
     Write-Host ""
 }
 
-function pe_DollarSign {
+function pe_DollarSign { <#DollarSign #>
     Write-Host "$" -NoNewLine
 }
 
-function pe_Lamda {
+function pe_Lamda { <#Lamda #>
     Write-Host "λ" -NoNewLine
 }
 
-function pe_GreaterThan{
+function pe_GreaterThan{ <#GreaterThan#>
     Write-Host ">" -NoNewLine
 }
 
-function pe_BracketedTime{
+function pe_BracketedTime{ <#BracketedTime#>
     Write-Host -ForeGroundColor $script:promptColors["Time"] -NoNewLine "[$(Get-Date -Format "ddd MMM dd HH:mm:ss")]"
 }
 
-function pe_PoshGitStatus{
+function pe_PoshGitStatus{ <#PoshGitStatus#>
     if(Get-Module posh-git)
     {
         Write-VcsStatus
